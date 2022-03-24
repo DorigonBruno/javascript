f507bf70a8143260664a00510f7911ea330816c9
DorigonBruno <96551135+DorigonBruno@users.noreply.github.com>
2022-03-24 18:23:55 -0300

    new commit

--- exercicios/ex007_condicoes/condicao_composta.js
+++
@@ -1,7 +0,0 @@
-var pais = 'itália'
-console.log(`Vivendo em ${pais}`)
-if(pais != 'Brasil'){
-    console.log(`Vc é estrangeiro`)
-}else{
-    console.log(`Vc é Brasileiro`)
-}
--- exercicios/ex007_condicoes/condicao_simples.js
+++
@@ -1,8 +0,0 @@
-var vel = 70
-console.log(`A velocidade do seu carro é ${vel}km/h`)
-
-if(vel > 60){ //condição simples
-    console.log(`Vc ultrapassou a velocidade permitida.MULTADO!`)
-}
-
-console.log(`Dirija sempre usando cinto de segurança`)
