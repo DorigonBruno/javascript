5ac67dc5f9bb8683685a3f3a3fc0e87ea2e429d7
DorigonBruno <96551135+DorigonBruno@users.noreply.github.com>
2022-03-29 16:38:41 -0300

    coments

--- exercicios/aula012_expraticos/ex014_expraticos/script.js
+++ exercicios/aula012_expraticos/ex014_expraticos/script.js
@@ -2,7 +2,7 @@ function carregar(){
     var msg = document.querySelector('div#msg')
     var img = document.getElementById('imagem')
     var data = new Date()
-    var hora = 20 //data.getHours()
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if(hora >= 0 && hora < 12){
         msg.innerHTML += "<p><strong>Bom dia</strong></p>"
--- exercicios/aula012_expraticos/exe015_expraticos/script.js
+++ exercicios/aula012_expraticos/exe015_expraticos/script.js
@@ -1,18 +1,18 @@
 function verificar(){
-    var data = new Date()
-    var ano = data.getFullYear()
+    var data = new Date() // para receber a data do usuario.
+    var ano = data.getFullYear() // para receber o ano do usuário.
     var year = document.getElementById('txtn')
     var res = document.getElementById('res')
     if(year.value.length == 0 || year.value > ano){
         alert('[ERRO] dados incorretos')
     } else{
-       var fsex = document.getElementsByName('boxr')
+       var fsex = document.getElementsByName('boxr') // essas variaveis são no else pq,caso não der erro ele executa o código.
        var idade = Number(ano - year.value)
        var genero = ''
-       var img = document.createElement('img')
-       img.setAttribute('id', 'foto')
+       var img = document.createElement('img') // variavel para criar uma imagem
+       img.setAttribute('id', 'foto') // aqui selecionamos a variavel e demos uma id 'foto'
        if(idade >= 0 && idade < 10){
-           img.setAttribute('src', 'bebe-h.png')
+           img.setAttribute('src', 'bebe-h.png') // aqui colocamos a imagem src para aparecer na tela conforme o if.
            //criança
        } else if(idade < 26){
             img.setAttribute('src', 'jovem-h.png')
@@ -42,8 +42,8 @@ function verificar(){
         //idoso
         }
     }
-       res.innerHTML = `<p>Detectamos ${genero} com ${idade} anos.</p>`
-       res.appendChild(img)
-       res.style.textAlign = 'center'
+       res.innerHTML = `<p>Detectamos ${genero} com ${idade} anos.</p>` // para dar o resultado.
+       res.appendChild(img) // aqui para aparecer a imagem na tela.
+       res.style.textAlign = 'center' // para centralizar o texto da div 'res'
     }
 }
